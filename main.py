53f7f07f7b8e3bf6472412d80d98e100def553ca
harshrevar1102 <harshrevar1102@gmail.com>
2026-07-02 11:57:09 +0530

    Calculating execution time and time to first token

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import ollama
+import time
+
+start_time = time.perf_counter()
 
 response = ollama.chat(
     model="phi3:mini",
@@ -11,9 +14,20 @@ response = ollama.chat(
     stream = True,
 )
 
+
 # print(response['message']['content'])
 
 # print(response.message.content)
 
+first_chunk = True
+
 for chunk in response:
+  if first_chunk:
+    ttft = time.perf_counter() - start_time
+    first_chunk = False
   print(chunk['message']['content'], end='', flush=True)
+
+end_time = time.perf_counter()
+
+print (f"\n\nExecution time: {end_time - start_time:.4f} seconds")
+print (f"\nTime to first token: {ttft:.4f} seconds")
