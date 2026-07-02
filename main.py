22c8162908edef9076345b4f95bc2d91c7888c4c
harshrevar1102 <harshrevar1102@gmail.com>
2026-07-02 12:26:35 +0530

    computed tokens per second

--- main.py
+++ main.py
@@ -20,14 +20,24 @@ response = ollama.chat(
 # print(response.message.content)
 
 first_chunk = True
+last_chunk = None
 
 for chunk in response:
   if first_chunk:
     ttft = time.perf_counter() - start_time
     first_chunk = False
+  
+  last_chunk = chunk  
   print(chunk['message']['content'], end='', flush=True)
 
 end_time = time.perf_counter()
 
+output_tokens = last_chunk.eval_count
+generation_time = last_chunk.eval_duration
+seconds = generation_time / 1_000_000_000
+
+TPS = output_tokens / seconds
+
 print (f"\n\nExecution time: {end_time - start_time:.4f} seconds")
 print (f"\nTime to first token: {ttft:.4f} seconds")
+print (f"\nTokens per second: {TPS:.2f}")
